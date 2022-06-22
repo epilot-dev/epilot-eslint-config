6c8e98ab5517947db96abda82ad9e7ea207bc336
josua.blejeru <j.blejeru@epilot.cloud>
2022-06-22 17:27:02 +0300

    delete old experimental feature

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,7 +50,6 @@ module.exports = {
     'no-undef': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars-experimental': 'error',
     'spaced-comment': ['error', 'always', { exceptions: ['-', '+'] }],
     'prettier/prettier': [
       'error',
